0dbf0f782bdf65a067ae05f92a2857f3a608218b
kevincalixte <kevin.calixte@icloud.com>
2025-12-05 21:07:39 +0100

    body: bg color

--- src/app/components/NavbarTop/NavbarTop.tsx
+++ src/app/components/NavbarTop/NavbarTop.tsx
@@ -4,7 +4,7 @@ import { PiSignInLight } from "react-icons/pi";
 function NavbarTop() {
   return (
     <>
-      <nav className=' flex items-center p-5 text-white absolute w-full bg-linear-to-b from-black/70 to-transparent'>
+      <nav className=' flex items-center p-5 text-white absolute w-full bg-linear-to-b from-[#070301]/80 to-transparent'>
         <Link href={""} className='flex-1 z-10'><img className='w-10' src="/img/logos/wivy-app-logo-transparent.png" alt="" /></Link>
          <Link href={''} className="hidden lg:flex items-center gap-1 text-[1.2rem]">Sign in <PiSignInLight></PiSignInLight></Link>
       </nav>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#070301] `}
       >
         <NavbarTop></NavbarTop>
         <Home></Home>

68e46742a7cb04ef9617574766a1d966656e44f8
kevincalixte <kevin.calixte@icloud.com>
2025-12-07 23:27:37 +0100

    welcome: user test

--- src/app/components/NavbarTop/NavbarTop.tsx
+++ src/app/components/NavbarTop/NavbarTop.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { PiSignInLight } from "react-icons/pi";
 import { usePathname } from 'next/navigation';
+import { FaRegEye } from "react-icons/fa";
 
 function NavbarTop() {
   const pathname = usePathname();
@@ -13,13 +14,13 @@ function NavbarTop() {
         id='navbartop'
         className={
           isWelcome
-            ? ''
+            ? 'flex items-center p-3 fixed w-[90%] rounded-4xl mt-5 h-10 bg-white/20 text-white'
             : 'flex items-center p-5 text-white absolute w-full bg-linear-to-b from-black to-transparent'
         }
       >
-
         <Link href={'/'} className='flex-1 z-10'><img className='w-10' src={isWelcome ? '' : "/img/logos/wivy-app-logo-transparent.png"} alt="" /></Link>
         <Link href={''} className="hidden lg:flex items-center gap-1 text-[1.2rem]">Sign Up <PiSignInLight /></Link>
+        <Link href={''} className=''><FaRegEye></FaRegEye></Link>
       </nav>
     </div>
   );
--- src/app/components/User/User.tsx
+++ src/app/components/User/User.tsx
@@ -1,141 +1,155 @@
 import React from 'react'
 import Tag from '../Tag/Tag';
-
+import {
+    GiAries,
+    GiTaurus,
+    GiGemini,
+    GiCancer,
+    GiLeo,
+    GiVirgo,
+    GiLibra,
+    GiScorpio,
+    GiSagittarius,
+    GiCapricorn,
+    GiAquarius,
+    GiPisces
+} from "react-icons/gi";
 function User() {
     return (
-        <div className='mt-5 mb-22'>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
-        <div className='flex justify-center'>
-            <article className='flex w-[80%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
-                <div className='w-1/3 h-full rounded-2xl overflow-hidden'><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
-                <div className='flex gap-2 px-4 py-1'>
-                    <h2>Theo,</h2>
-                    <span>25</span>
-                    {/* <span className=''>Signe astrologique</span> */}
-                </div>
-                {/* <Tag></Tag> */}
-            </article>
-        </div>
+        <div className='mt-18 mb-22'>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/2  justify-center  h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex  ir  w-full gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25 </span>
+                        <GiAries className='' />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
+            <div className='flex justify-center'>
+                <article className='flex w-[85%] h-24 bg-white/15 m-3 rounded-2xl overflow-hidden'>
+                    <div className='w-1/3 h-full rounded-2xl  '><img className='w-full h-full object-cover' src="/img/home/album-meet/meet1.jpg" alt="" /></div>
+                    <div className='flex gap-2 px-4 py-1'>
+                        <h2>Theo,</h2>
+                        <span>25</span> <GiAries />
+                        {/* <span className=''>Signe astrologique</span> <GiAries/> */}
+                    </div>
+                    {/* <Tag></Tag> */}
+                </article>
+            </div>
         </div>
     )
 }

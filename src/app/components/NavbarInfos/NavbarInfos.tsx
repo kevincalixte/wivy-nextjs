59ab210aa52ff12bd89967708f0da859248d3612
kevincalixte <kevin.calixte@icloud.com>
2025-12-08 01:56:55 +0100

    welcome: navbarinfos test

---
+++ src/app/components/NavbarInfos/NavbarInfos.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavbarInfos() {
+    return (
+        <div id='navbarinfos-container' className='flex justify-center'>
+            <nav
+                id='navbarinfos'
+                className=
+                'flex items-center p-3 fixed w-[60%] rounded-4xl mt-20 h-10 bg-white/20 text-white'
+            >
+            </nav>
+        </div>
+    )
+}
+
+export default NavbarInfos
--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -2,11 +2,14 @@
 import React from 'react';
 import NavbarBottom from '../components/NavbarBottom/NavbarBottom';
 import User from '../components/User/User';
+import NavbarInfos from '../components/NavbarInfos/NavbarInfos';
 
 function welcome() {
 
   return (
     <>
+    {/* <NavbarInfos></NavbarInfos> */}
+    
       <div className='text-white'>
         {/* Configuration du profil */}
         {/* Page principale 'if last_mode is list'*/}

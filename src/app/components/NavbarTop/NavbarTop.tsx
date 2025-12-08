c0c09a5f87c96882cb5e8671c3ed1a107ad4b614
kevincalixte <kevin.calixte@icloud.com>
2025-12-09 00:11:52 +0100

    messages: navbar added

--- src/app/components/NavbarTop/NavbarTop.tsx
+++ src/app/components/NavbarTop/NavbarTop.tsx
@@ -9,28 +9,48 @@ import { FiSearch } from 'react-icons/fi';
 function NavbarTop() {
   const pathname = usePathname();
   const isWelcome = pathname === '/welcome';
+  const isMessages = pathname === '/messages';
   return (
+
+    // Si welcome : barre de recherche
+    // Si messages : messages + favoris
+    // Sinon : affichagge du logo
     <div id='navbartop-container' className='flex justify-center'>
       <nav
         id='navbartop'
         className={
           isWelcome
             ? 'flex items-center p-3 fixed w-[90%] backdrop-blur rounded-4xl mt-5 h-10 bg-white/20 text-white z-10'
-            : 'flex items-center p-5 text-white absolute w-full bg-linear-to-b from-black to-transparent'
+            : isMessages
+              ? 'absolute w-full  '
+              : 'flex items-center p-5 text-white absolute w-full bg-linear-to-b from-black to-transparent'
         }
       >
+        {/* Page Welcome */}
         <Link href={'/'} className=''>{isWelcome ?
           <span className='flex items-center gap-1.5 '>
             <FiSearch></FiSearch>
             <p className='text-xs opacity-50'>What are you looking for today ? </p>
           </span>
           : ''}</Link>
+
+        {/* Page Messages */}
+        <Link href={'/'} className=''>{isMessages ?
+          <div className='flex justify-evenly p-5 '>
+            <span className=''>Messages</span>
+            <span className=''>Favorites</span>
+          </div>
+          : ''}</Link>
+
         <Link href={'/'} className='flex-1 z-10'>
-          {!isWelcome && (
+          {/* Ne pas afficher le logo pour ces pages */}
+          {!(isWelcome || isMessages) && (
             <img className='w-10' src="/img/logos/wivy-app-logo-transparent.png" alt="" />
           )}
+
         </Link>
         <Link href={''} className='hidden lg:flex items-center gap-1 text-[1.2rem]'>Sign Up <PiSignInLight /></Link>
+        {/* Si welcome: logo visites */}
         {isWelcome ? <FaRegEye /> : null}
       </nav>
     </div>
--- src/app/components/UserMessage/UserMessage.tsx
+++ src/app/components/UserMessage/UserMessage.tsx
@@ -1,13 +1,10 @@
 'use client'
-import React, {useState} from 'react'
 import { BsFillReplyFill } from "react-icons/bs";
 import { VscVerifiedFilled } from "react-icons/vsc";
 function UserMessage() {
 
-    const [isMessageOpen, setIsMessageOpen] = useState(true);
-
     return (
-        <div className='min-h-screen pt-20 w-screen  '>
+        <div className='min-h-screen pt-15 w-screen  '>
             <div className='flex items-center px-5 py-3 '>
                 <div className="w-18 h-18 shrink-0 rounded-full overflow-hidden"><img className="w-full h-full object-cover" src="/img/home/album-meet/meet2.jpg" alt="" /></div>
                 <div className='flex flex-col  w-full pl-3'>

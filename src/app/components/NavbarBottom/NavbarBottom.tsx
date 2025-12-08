9f36394bfe903b02a05ec724fcc60c0d70fe94c3
kevincalixte <kevin.calixte@icloud.com>
2025-12-08 13:32:05 +0100

    welcome: navbarbottom selected

--- src/app/components/NavbarBottom/NavbarBottom.tsx
+++ src/app/components/NavbarBottom/NavbarBottom.tsx
@@ -1,17 +1,18 @@
 import Link from 'next/link'
-import { FiSearch, FiStar,FiUsers, FiHeart, FiMessageCircle, FiMessageSquare, FiHome, FiUser } from 'react-icons/fi';
+import { FiSearch, FiStar, FiUsers, FiHeart, FiMessageCircle, FiMessageSquare, FiHome, FiUser } from 'react-icons/fi';
 import { IoMdSync } from "react-icons/io";
 
 function NavbarBottom() {
-  
+
   return (
-    
+
     <div className='flex justify-center'>
       <nav className=' flex items-center p-3 fixed w-[90%] backdrop-blur rounded-4xl bottom-0 mb-5 h-15 bg-white/10 text-white'>
         <ul className='flex gap-3 justify-evenly w-full text-2xl'>
+          {/* Selected :  bg-white/20 rounded-2xl p-2 */}
           <li><Link href={""} className="flex items-center gap-1"><FiHome /></Link></li>
-          <li><Link href={""} className="flex items-center gap-1"><FiMessageSquare /></Link></li>
           <li><Link href={""} className="flex items-center gap-1"><FiStar /></Link></li>
+          <li><Link href={""} className="flex items-center gap-1"><FiMessageSquare /></Link></li>
           <li><Link href={""} className="flex items-center gap-1">< IoMdSync /></Link></li>
           <li><Link href={""} className="flex items-center gap-1">< FiUser /></Link></li>
         </ul>
--- src/app/components/NavbarInfos/NavbarInfos.tsx
+++ src/app/components/NavbarInfos/NavbarInfos.tsx
@@ -6,10 +6,10 @@ function NavbarInfos() {
             <nav
                 id='navbarinfos'
                 className=
-                'flex items-center justify-between pr-3 fixed w-[70%] rounded-4xl mt-20 h-10 bg-white/20 text-white z-10 backdrop-blur'
+                'flex items-center justify-between pr-6 fixed w-[70%]  rounded-4xl mt-20 h-10 bg-white/20 text-white z-10 backdrop-blur'
             >
-                <div className='rounded-l-2xl overflow-hidden'><img className='w-full h-10 object-cover' src="/img/home/album-meet/meet2.jpg" alt="" /></div>
-                <p className='text-sm'>Justine visited your profile</p>
+                <div className='rounded-l-2xl w-1/4 overflow-hidden'><img className='w-full h-10 object-cover' src="/img/home/album-meet/meet2.jpg" alt="" /></div>
+                <p className='text-xs w-3/4 text-center'>Justine visited your profile</p>
             </nav>
         </div>
     )
